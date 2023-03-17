d8697e2b20124b86fdfc91a920392b860699d83d
Leandro Real <leo.real2585@gmail.com>
2023-03-16 21:07:02 -0300

    feat: add retry pattern

--- src/app/features/students/students.service.ts
+++ src/app/features/students/students.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Student } from './student';
 import { environment } from 'src/environments/environment';
 import { NgxSpinnerService } from 'ngx-spinner/public_api';
 import { ToastrService } from 'ngx-toastr/public_api';
-import { finalize, delay } from 'rxjs/operators'
+import { finalize, delay, catchError, retryWhen, mergeMap, scan } from 'rxjs/operators'
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { Observable } from 'rxjs/internal/Observable';
+import { of } from 'rxjs/internal/observable/of';
 
 @Injectable({
   providedIn: 'root'
@@ -18,27 +21,45 @@ export class StudentsService {
   }
 
   save(student: Student) {
-    return this.http.post(`${this.apiURL}`, student);
+    return this.http.post(`${this.apiURL}`, student).pipe(
+      delay(2000),
+      catchError(error => this.exceptionHandler(error)),
+      finalize(() => this.spinner.hide())
+    );
 
   }
 
   update(id: number, student: Student) {
-    return this.http.put(`${this.apiURL}/${id}`, student);
+    return this.http.put(`${this.apiURL}/${id}`, student).pipe(
+      delay(2000),
+      catchError(error => this.exceptionHandler(error)),
+      finalize(() => this.spinner.hide())
+    );
 
   }
 
   deleteById(id: number) {
-    return this.http.delete<void>(`${this.apiURL}/${id}`);
+    return this.http.delete<void>(`${this.apiURL}/${id}`).pipe(
+      delay(2000),
+      catchError(error => this.exceptionHandler(error)),
+      finalize(() => this.spinner.hide())
+    );
   }
 
   findById(id: number) {
-    return this.http.get<Student>(`${this.apiURL}/${id}`);
+    return this.http.get<Student>(`${this.apiURL}/${id}`).pipe(
+      delay(2000),
+      catchError(error => this.exceptionHandler(error)),
+      finalize(() => this.spinner.hide())
+    );
   }
 
   findAll() {
     this.spinner.show();
     return this.http.get<Student[]>(`${this.apiURL}`).pipe(
       delay(2000),
+      retryWhen(err => this.retryHandler(err)),
+      catchError(error => this.exceptionHandler(error)),
       finalize(() => this.spinner.hide())
     );
   }
@@ -54,4 +75,27 @@ export class StudentsService {
     // this.students.push(new Student(8, 'Bernardo', 'bernardo@gmail.com', '1995-02-27'));
   }
 
+  private exceptionHandler(error: HttpErrorResponse) {
+    this.toastr.error(error.message, `${error.status} - ${error.statusText}`)
+    return throwError(error)
+  }
+
+  private retryHandler(error: Observable<any>) {
+    return error.pipe(
+      delay(2000),
+      mergeMap(err => {
+        if(err.status < 500) {
+          return of(err)
+        }
+        return throwError(err)
+      }),
+      scan((acc, err) => {
+        if(acc > 5) {
+          throw err;
+        }
+        this.toastr.warning(`Retrying the request #${acc}`, `${err.status} - Retrying`)
+        return ++acc
+      }, 1)
+    )
+  }
 }
